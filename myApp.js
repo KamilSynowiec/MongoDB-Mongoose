6dd185142313a588f8ea6f5e47276be87f50f46b
Kamil Synowiec <1935002@brunel.ac.uk>
2022-07-19 20:46:32 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,7 +3,12 @@ require('dotenv').config();
 const mongoose=require('mongoose');
 mongoose.connect(process.env.MONGO_URI);
 
-let Person;
+const Schema = mongoose.Schema;
+const personSchema = new Schema({
+name: {type: String, required: true},
+age: Number,
+favoriteFoods: [String]
+});
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
