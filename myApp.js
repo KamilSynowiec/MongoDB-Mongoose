5c745d95cad22bf5a52b2263a1a0ce0cbe60b1c5
Kamil Synowiec <1935002@brunel.ac.uk>
2022-07-20 17:15:09 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -54,7 +54,15 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  People.find({name: personName}, function(err, personFound){
+    if(err){
+      return console.log(err);
+    }
+    else
+    {
+      done(null, personFound);
+    }
+  });
 };
 
 const findOneByFood = (food, done) => {
