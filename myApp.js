62624792c158ba5d61ef255e3c01fada8df148e2
Kamil Synowiec <1935002@brunel.ac.uk>
2022-07-24 15:07:39 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -133,7 +133,10 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({age: 0}).exec(function(error, people)=>{
+       if(error) console.log(error);
+       done(null, people);
+  });
 };
 
 /** **Well Done !!**
